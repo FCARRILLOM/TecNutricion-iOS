9b6bf90ecab9fe8cc55f1867ac9199146603c02e
fcarrillom <fcm238@gmail.com>
2020-04-20 20:49:57 -0500

    Agregado vista de Mi Plan con los diferentes grupos de alimentos

---
+++ TecNutricion-iOS/Model/GpoAlimenticio.swift
@@ -0,0 +1,14 @@
+//
+//  GrupoAlimenticio.swift
+//  TecNutricion-iOS
+//
+//  Created by Fernando Carrillo on 20/04/20.
+//  Copyright © 2020 FernandoCarrillo. All rights reserved.
+//
+
+import Foundation
+
+enum GpoAlimenticio {
+    case agua
+    case proteina
+}
---
+++ TecNutricion-iOS/View/MiPlanTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MiPlanTableViewCell.swift
+//  TecNutricion-iOS
+//
+//  Created by Fernando Carrillo on 20/04/20.
+//  Copyright © 2020 FernandoCarrillo. All rights reserved.
+//
+
+import UIKit
+
+class MiPlanTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
