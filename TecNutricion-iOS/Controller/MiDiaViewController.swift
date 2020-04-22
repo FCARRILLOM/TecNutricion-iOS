e400daa9304e0e0337ae17fa7f0418d7d728d1ab
Carlos Estrada <carlos.deg02@hotmail.com>
2020-04-22 15:50:55 -0500

    Agregue alert para redireccionar usario a MiPlan si no hay plan

--- TecNutricion-iOS/Controller/MiDiaViewController.swift
+++ TecNutricion-iOS/Controller/MiDiaViewController.swift
@@ -26,14 +26,22 @@ class MiDiaViewController: UIViewController, UICollectionViewDataSource, UIColle
         let menuButtonItem = UIBarButtonItem(title: "Menu", style: .plain, target: self, action: #selector(toggleMenu))
         navigationItem.leftBarButtonItem = menuButtonItem
         
+        view.backgroundColor = UIColor.white
         createGroups()
-
         setupCollectionView()
 
-        view.backgroundColor = UIColor.white
     }
 
-     func createGroups(){
+    func createGroups(){
+
+        planLista = loadPlan() 
+
+        if !checkValidPlan(planLista) {
+            let alert = UIAlertController(title: "Error", message: "Aun no hay un plan registrado", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ir a Mi Plan", style: .cancel, handler: nil))
+            present(alert, animated: true, completion: sendToPlan)
+        }
+        
         listaGpos = [
             GpoAlimenticio(name: "Vegetales", icon: "Apple", portions: 0),
             GpoAlimenticio(name: "Carnes", icon: "Apple", portions: 0),
@@ -42,6 +50,10 @@ class MiDiaViewController: UIViewController, UICollectionViewDataSource, UIColle
             GpoAlimenticio(name: "Leguminosas", icon: "Apple", portions: 0),
             GpoAlimenticio(name: "Frutas", icon: "Apple", portions: 0),
             GpoAlimenticio(name: "Grasas", icon: "Apple", portions: 0),]
+    }
+
+    func sendToPlan() {
+        delegate?.handleSectionTap(section: MenuSection.MiPlan)
     }
     
     // MARK: - Table View
@@ -80,5 +92,33 @@ class MiDiaViewController: UIViewController, UICollectionViewDataSource, UIColle
     // Enseña o esconde el menu
     @objc func toggleMenu() {
         delegate?.handleMenuToggle()
+    }
+
+    // MARK: Save/Load Data
+
+    func loadPlan() -> [GpoAlimenticio] {
+        do {
+            let data = try Data.init(contentsOf: dataFileURL())
+            let newListaGpos = try JSONDecoder().decode([GpoAlimenticio].self, from: data)
+            return newListaGpos
+        }
+        catch {
+            print("Error loading mi plan data")
+            return []
+        }
+    }
+
+    func checkValidPlan(plan: [GpoAlimenticio]) -> Bool {
+        if plan.count == 0  {
+            return false
+        }
+
+        for g in plan {
+            if g.protions > 0 {
+                return true
+            }
+        }
+
+        return false
     }
 }
