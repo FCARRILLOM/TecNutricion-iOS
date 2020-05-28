54a2ff782cae0a8e67951ee6ab58f31344c57800
Carlos Estrada <carlos.deg02@hotmail.com>
2020-05-27 20:20:20 -0700

    Agregue nuevos iconos de azucar y grasa

--- TecNutricion-iOS/Model/GpoAlimenticio.swift
+++ TecNutricion-iOS/Model/GpoAlimenticio.swift
@@ -31,11 +31,11 @@ class GpoAlimenticio: Equatable, Codable, CustomStringConvertible {
         return [
             GpoAlimenticio(name: "Vegetales", icon: "vegetable-icon-color", portions: 0),
             GpoAlimenticio(name: "Carnes", icon: "meat-icon-color", portions: 0),
-            GpoAlimenticio(name: "Azucares", icon: "sugar-icon-color", portions: 0),
+            GpoAlimenticio(name: "Azucares", icon: "sugar-2", portions: 0),
             GpoAlimenticio(name: "Cereales", icon: "cereal-icon-color", portions: 0),
             GpoAlimenticio(name: "Leguminosas", icon: "pea-icon-color", portions: 0),
             GpoAlimenticio(name: "Frutas", icon: "apple-icon-color", portions: 0),
-            GpoAlimenticio(name: "Grasas", icon: "fat-icon-color", portions: 0),
+            GpoAlimenticio(name: "Grasas", icon: "fat-2", portions: 0),
             GpoAlimenticio(name: "Leche", icon: "milk-icon-color", portions: 0),
             GpoAlimenticio(name: "Agua (1 vaso = 250ml)", icon: "water-icon-color", portions: 0),
         ]
